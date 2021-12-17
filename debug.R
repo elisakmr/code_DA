fe3869369afc7f51c1845fce049342b9b80d7345
elisa <50920419+elisakmr@users.noreply.github.com>
2021-12-17 17:43:16 +0100

    ALL POLY

--- debug.R
+++ debug.R
@@ -12,3 +12,27 @@ sum((lsm_utm %>% filter(metric=='pland'))$value)
 
 head(lsm_latlon)
 head(lsm_utm)
+
+
+list_lsm <- list()
+list_buf <- list()
+
+for (i in 1:200){
+  
+  WD.tmp = list.files(path=dir_data, pattern = paste0("Basin_",i,"_"), full.names = TRUE)
+  
+  list_buf[[i]] <- get(load(file=file.path(WD.tmp, paste0(year, "_mapbiomas_lsm_buf",tampon, "_",i,".RData")))) 
+  
+
+  
+  
+  
+}
+
+which(is.na(list_buf))
+df_lsm <- do.call("rbind",list_lsm)
+df_buf <- do.call("rbind",list_buf)
+dim(df_buf)
+dim(df_lsm)
+
+which(list_buf=="nonascente")
